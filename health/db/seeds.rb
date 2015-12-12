ccbf81e800c14c3915c35981d124b40cbc8e0a85
Robert <rbezaire@mail.csuchico.edu>
2015-12-12 13:54:36 -0800

    production mode set up, seed file set up and database migrated and seeded

--- health/db/seeds.rb
+++ health/db/seeds.rb
@@ -1,31 +1,252 @@
 users = User.create(
   [
-    { email: 'a@a.com', name: "Mr. Aardvark", password: "a", password_confirmation: "a"},
-    { email: 'b@b.com', name: "Mr. Bear", password: "b", password_confirmation: "b"},
-    { email: 'c@c.com', name: "Mr. Cat", password: "c", password_confirmation: "c"},
-    { email: 'd@d.com', name: "Mr. Dog", password: "d", password_confirmation: "d"},
-    { email: 'e@e.com', name: "Mr. Elephant", password: "e", password_confirmation: "e"},
-    { email: 'f@f.com', name: "Mr. Fox", password: "f", password_confirmation: "f"},
-    { email: 'g@g.com', name: "Mr. Gopher", password: "g", password_confirmation: "g"},
-    { email: 'h@h.com', name: "Mr. Horse", password: "h", password_confirmation: "h"},
-    { email: 'i@i.com', name: "Mr. Iguana", password: "i", password_confirmation: "i"},
-    { email: 'j@j.com', name: "Mr. Jackrabbit", password: "j", password_confirmation: "j"},
-    { email: 'k@k.com', name: "Mr. Kangaroo", password: "k", password_confirmation: "k"},
-    { email: 'l@l.com', name: "Mr. Llama", password: "l", password_confirmation: "l"},
-    { email: 'm@m.com', name: "Mr. Mouse", password: "m", password_confirmation: "m"},
-    { email: 'n@n.com', name: "Mr. Numbat", password: "n", password_confirmation: "n"},
-    { email: 'o@o.com', name: "Mr. Opossum", password: "o", password_confirmation: "o"},
-    { email: 'p@p.com', name: "Mr. Peacock", password: "p", password_confirmation: "p"},
-    { email: 'q@q.com', name: "Mr. Quail", password: "q", password_confirmation: "q"},
-    { email: 'r@r.com', name: "Mr. Rabbit", password: "r", password_confirmation: "r"},
-    { email: 's@s.com', name: "Mr. Snake", password: "s", password_confirmation: "s"},
-    { email: 't@t.com', name: "Mr. Tiger", password: "t", password_confirmation: "t"},
-    { email: 'u@u.com', name: "Mr. Urial", password: "u", password_confirmation: "u"},
-    { email: 'v@v.com', name: "Mr. Vulture", password: "v", password_confirmation: "v"},
-    { email: 'w@w.com', name: "Mr. Wombat", password: "w", password_confirmation: "w"},
-    { email: 'x@x.com', name: "Mr. Xerox", password: "x", password_confirmation: "x"},
-    { email: 'y@y.com', name: "Mr. Yak", password: "y", password_confirmation: "y"},
-    { email: 'z@z.com', name: "Mr. Zebra", password: "z", password_confirmation: "z"},
+    { email: 'a@a.com', password: "a", password_confirmation: "a"},
+    { email: 'b@b.com', password: "b", password_confirmation: "b"},
+    { email: 'c@c.com', password: "c", password_confirmation: "c"},
+    { email: 'd@d.com', password: "d", password_confirmation: "d"},
+    { email: 'e@e.com', password: "e", password_confirmation: "e"},
+    { email: 'f@f.com', password: "f", password_confirmation: "f"},
+    { email: 'g@g.com', password: "g", password_confirmation: "g"},
+    { email: 'h@h.com', password: "h", password_confirmation: "h"},
+    { email: 'i@i.com', password: "i", password_confirmation: "i"},
+    { email: 'j@j.com', password: "j", password_confirmation: "j"},
+    { email: 'k@k.com', password: "k", password_confirmation: "k"},
+    { email: 'l@l.com', password: "l", password_confirmation: "l"},
+    { email: 'm@m.com', password: "m", password_confirmation: "m"},
+    { email: 'n@n.com', password: "n", password_confirmation: "n"},
+    { email: 'o@o.com', password: "o", password_confirmation: "o"},
+    { email: 'p@p.com', password: "p", password_confirmation: "p"},
+    { email: 'q@q.com', password: "q", password_confirmation: "q"},
+    { email: 'r@r.com', password: "r", password_confirmation: "r"},
+    { email: 's@s.com', password: "s", password_confirmation: "s"},
+    { email: 't@t.com', password: "t", password_confirmation: "t"},
+    { email: 'u@u.com', password: "u", password_confirmation: "u"},
+    { email: 'v@v.com', password: "v", password_confirmation: "v"},
+    { email: 'w@w.com', password: "w", password_confirmation: "w"},
+    { email: 'x@x.com', password: "x", password_confirmation: "x"},
+    { email: 'y@y.com', password: "y", password_confirmation: "y"},
+    { email: 'z@z.com', password: "z", password_confirmation: "z"},
   ]
 )
 
+recipes = Recipe.create(
+  [
+    {recipe_name: "Maple Pomegranate Glazed Short Ribs", ingredients: "
+Ingredients\r\n\r\n
+
+    3 pounds short ribs (that gave me 6 short ribs)\r\n
+    himalayan sea salt, to taste\r\n
+    garlic powder, to taste\r\n
+    powdered ginger, to taste\r\n
+    3 tablespoons Tin Star Ghee\r\n
+    1 red onion, thinly sliced\r\n
+    3 garlic cloves, minced\r\n
+    3-4 cups beef broth\r\n
+    1 cup pomegranate juice\r\n
+    ¼ cup coconut aminos\r\n
+    3 tablespoons maple syrup\r\n\r\n
+
+For the glaze\r\n\r\n
+
+    2 cup pomegranate juice\r\n
+    ¼ cup maple syrup\r\n
+    ¼ teaspoon powdered ginger\r\n\r\n
+
+For the garnishes\r\n\r\n
+
+    pomegranate seeds\r\n
+    sliced green onions\r\n\r\n
+
+Instructions\r\n\r\n
+
+   1. Preheat oven to 325 degrees F. Place a large dutch oven over medium heat. Add 3 tablespoons of ghee. Salt short ribs on all sides then sprinkle with a bit of garlic powder and ginger. Brown the ribs on all sides, about 1-2 minutes per side. Remove and set aside.\r\n
+   2. Add onion and garlic to the pan and cook for about 2 minutes until onions become translucent. Add the broth, pomegranate juice, coconut aminos and maple syrup to the dutch oven and scrape the bottom of the pan with a wooden spatula then cover and let the pot come to boil.\r\n
+   3. Once boiling, add the short ribs back to the pot into the liquid so they are completely submerged. Put lid on the dutch oven and into the preheated oven. Cook until the ribs are falling off the bone, about 3 hours. Remove the pan from the oven and let rest for 30 minutes with lid still on.\r\n
+   4. When the ribs have 30 minutes to go, place a large sauté pan over medium heat. Add pomegranate juice, maple syrup and ginger to the pan and let bring to a low boil, whisking every couple minutes. The mixture will slowly start to reduce and thicken and in about 30 minutes, it will coat the back of the spoon. Be sure to not overcook because the mixture can become too thick and almost harden.\r\n
+   5. Carefully remove short ribs from liquid, then top with glaze, pomegranate seeds and green onions.\r\n"},
+
+	{recipe_name: "Slow Cooker Pot Roast With Easy Gravy", ingredients: "
+Ingredients\r\n\r\n
+
+    2 tablespoons Tin Star Ghee\r\n
+    4 pound chuck roast, tied around the outside with twine\r\n
+    Primal Palate Himalayan Sea Salt, to taste\r\n
+    3 medium carrots, peeled and sliced\r\n
+    3 medium parsnips, peeled and sliced\r\n
+    1 medium yellow onion, cut into quarters\r\n
+    3 garlic cloves, sliced\r\n
+    1 tablespoon chopped fresh thyme + extra sprigs\r\n
+    pinch of black pepper\r\n
+    2 Primal Palate Bay Leaves\r\n
+    2 cups beef broth\r\n
+    ⅓ cup red wine (optional)\r\n
+    1 tablespoon honey\r\n
+    ¼ cup tapioca or arrowroot flour\r\n
+    1 batch of Cauliflower Puree on page 202 in Juli Bauer's Paleo Cookbook\r\n\r\n
+
+Instructions\r\n\r\n
+
+    1. Place a large cast iron skillet over medium heat. Once hot, add ghee. Sprinkle salt on chuck roast then sear on all sides, about 2-3 minutes per side. Place in slow cooker and put sliced carrots, parsnips and onion around the roast. Top the roast with garlic cloves, a bit more salt, thyme and thyme sprigs and black pepper. Put the bay leaves in the pot then pour in the beef broth and red wine. Cover and let cook for 8 hours on low.\r\n
+    2. Once roast is done cooking, remove roast and place in a baking dish then use a slotted spoon to remove all the veggies and set in the baking dish with roast. Cover with foil to keep warm while you finish the dish.\r\n
+    3. If you are making cauliflower puree, make it now.\r\n
+    4. For the gravy: Dispose of bay leaves and thyme sprigs from slow cooker then use an immersion blender to puree broth in the bottom of the slow cooker. Once pureed, pour mixture into a large sauté pan and place it over medium heat. Add honey and whisk. Then while continuously mixing, add arrowroot or tapioca flour to the mixture, one teaspoon at a time. Be sure to continuously whisk because it can clump up. Once all flour is combined and gravy has thickened, pour mixture through a mesh strainer to get rid of any clumps.\r\n
+    5. To serve pot roast: place cauliflower puree in a bowl along with veggies, pot roast that has been pulled apart with fork, and gravy on top!\r\n"},
+
+	{recipe_name: "Butternut Squash Gnocchi with Sage Brown Butter", ingredients: "
+Ingredients\r\n\r\n
+
+    2 cups peeled and diced butternut squash\r\n
+    1-2 tablespoons Tin Star Ghee\r\n
+    pinch of garlic powder and salt\r\n
+    3 tablespoons Tin Star Browned Butter*\r\n
+    2 garlic cloves, minced\r\n
+    6-8 sage leaves\r\n
+    1 package of Cappello's Gnocchi\r\n
+    truffle salt, for garnishing\r\n
+    black pepper, for garnishing\r\n\r\n
+
+Instructions\r\n\r\n
+
+    1. Preheat oven to 375 degrees F.\r\n
+    2. Toss butternut squash in 1-2 tablespoons of ghee. Place butternut squash on a parchment paper lined baking sheet and sprinkle with garlic powder and salt. Roast butternut squash for 25-30 minutes until browned and fork tender.\r\n
+    3. When there is 10 minutes left for cooking the butternut squash, place a pot filled with water over medium high heat. Bring to boil.\r\n
+    4. While water heats up, place a medium cast iron skillet over medium heat and add 3 tablespoons of browned butter. Once hot, add garlic and sage leaves. Once sage leaves become crispy, about 2-3 minutes, place gnocchi in boiling water. Boil the gnocchi until they float (follow directions on package) then remove them with a slotted spoon and add them into the sage browned butter still over medium heat. Cook gnocchi on both sides for about 1 minutes then add in butternut squash and toss to coat everything in browned butter.\r\n
+    5. Sprinkle with a pinch of truffle salt and black pepper then eat up, gnocchi buttercup!\r\n
+"},
+	{recipe_name: "Merrymaker Paleo Cake", ingredients: "
+Ingredients\r\n\r\n
+For the base:\r\n
+
+    1 cup roasted hazelnuts\r\n
+    1 cup desiccated (shredded) coconut\r\n
+    ¼ cup raw cacao powder\r\n
+    3 tbs. coconut oil\r\n
+    1 tsp. vanilla\r\n
+    Pinch of salt\r\n\r\n
+
+For the filling:\r\n
+
+    1½ cups raw macadamias or cashews soaked\r\n
+    1½ cup coconut milk\r\n
+    ½ cup coconut oil\r\n
+    ¼ cup raw honey or maple syrup\r\n
+    1 vanilla bean\r\n
+    Salt\r\n
+
+For the toppings:\r\n
+
+    1 cup roast hazelnuts\r\n
+    ¼ cup coconut oil\r\n
+    ¼ cup raw cacao powder\r\n
+    1 tsp. vanilla\r\n
+    Salt\r\n
+    ¼ cup roast hazelnuts chopped\r\n
+    1 tbs. coconut sugar\r\n\r\n
+
+Instructions\r\n\r\n
+
+    1. Grease 16 silicone cupcake moulds with a little coconut oil.\r\n
+    2. Start with the base. In a food processor/blender, place all the ingredients and whiz until a smooth butter forms.  Place tablespoons of the base in to each mould and place in to the freezer.\r\n
+    3. Now to start with the filling. Drain and rinse the macadamias/cashews and place them in to a food processor/blender along with the coconut milk, coconut oil, sweetener, vanilla and salt\r\n
+    4. Whiz until completely smooth, this will take around 5 minutes.\r\n
+    5. Fill each mould with smooth vanilla filling.\r\n
+    6. It’s time to make this cake EPIC with all the toppings!\r\n
+    7. In a food processor/blender, place the roasted hazelnuts, coconut oil, raw cacao powder, vanilla and salt. Whiz until smooth. This will take around 5 minutes.\r\n
+    8. Place spoonfuls of the nutty-choc butter in to the centre of each cake.\r\n
+    9. In a small bowl mix the chopped hazelnuts and coconut sugar together.  Sprinkle a little on top of each cake and place in to the freezer for 2 hours or until set.\r\n
+    10. Turn out and enjoy! Store leftovers in the fridge or freezer and thaw out when you’re ready for a slice of Merrymaker Cake!\r\n
+    11. TIP: We like to drizzle ours with extra choc-nut butter!\r\n
+"},
+	{recipe_name: "Grilled Figs with Coconut Whipped Cream & Apple Cider Caramel", ingredients: "
+Ingredients\r\n\r\n
+For the apple cider caramel\r\n
+
+    ¾ cup heavy cream or full fat coconut milk\r\n
+    ¼ cup apple cider\r\n
+    1 cup maple sugar\r\n
+    1 teaspoon vanilla extract\r\n
+    ⅓ teaspoon ground cinnamon\r\n
+    1 cinnamon stick\r\n\r\n
+
+For the grilled figs\r\n
+
+    coconut whipped cream\r\n
+    1-2 tablespoons melted ghee\r\n
+    8-10 figs, halved\r\n
+    1 teaspoon maple sugar\r\n
+    ⅛ teaspoon ground cinnamon\r\n
+    mint leaves, for garnish\r\n\r\n
+
+Instructions\r\n\r\n
+
+    1. In a small saucepan over medium heat, combine all the ingredients for apple cider caramel. Stir until completely combined.\r\n
+    2. Bring to a boil, then set a timer for 12 minutes, being sure not to let the mixture boil over. After 12 minutes, reduce the heat to low and simmer for 5 more minutes or until the caramel coats the back of a spoon. Remove from the heat then let cool for 5 minutes then remove cinnamon stick before serving.\r\n
+    3. Make coconut whipped cream and set in the fridge to keep cool\r\n
+    4. Lastly, heat grill to a medium-high heat. Brush the inside of each fig half with melted ghee, sprinkle with a pinch of maple sugar and cinnamon on top, then place ghee side down on the hot grill. Grill for about 1 to 1½ minutes until grill marks appear.\r\n
+    5. Top each fig half 1 teaspoon of caramel, 1 teaspoon of whipped cream and a mint leaf to make it all cute and stuff.\r\n
+"},
+	{recipe_name: "Chipotle Smashed Eggs Benedict", ingredients: "
+Ingredients\r\n\r\n
+Chipotle Smashed Sweet Potatoes\r\n
+
+    1 large sweet potato or yam\r\n
+    2 tbsp olive oil\r\n
+    1 tsp sea salt\r\n
+    ½ tsp chipotle powder\r\n
+    ½ tsp garlic powder\r\n\r\n
+
+Hollandaise Sauce\r\n
+
+    3 egg yolks\r\n
+    2 tsp lemon juice\r\n
+    ¼ tsp sea salt\r\n
+    8 tbsp butter, melted\r\n\r\n
+
+For serving\r\n
+
+    8 poached eggs\r\n
+    8 slices bacon, cooked\r\n
+    2 cups spinach\r\n\r\n
+
+Instructions\r\n\r\n
+
+    1. Preheat the oven to 425 degrees F and line a baking sheet with parchment paper.\r\n
+    2. Bring a pot of water to a boil. Slice your sweet potatoes into thick rounds and boil them for 15 minutes. Drain completely of water.\r\n
+    3. Mix together your sea salt, chipotle and garlic to create a spice blend. Place the sweet potatoes onto the parchment lined baking sheet and use the back of a large spoon or bottom of a glass to lightly smash them. Drizzle the tops with olive oil and sprinkle with the chipotle salt mix. Flip to the other side and drizzle with the remaining olive oil and salt mix.\r\n
+    4. Roast the potatoes for 15 minutes.\r\n
+    5. Meanwhile, make the hollandaise sauce by adding the egg yolks, lemon juice and salt to a blender. Briefly blend until smooth. With the blender still running, slowly drizzle in the melted butter until your sauce begins to thicken.\r\n
+    6. Poach your eggs and cook the bacon, and sauté your spinach in the leftover bacon grease.\r\n
+    7. Plate with a smashed potato, some sautéed spinach, a slice of bacon and top with a poached egg and generous spoonful of Hollandaise sauce.\r\n
+"},
+	{recipe_name: "Grilled Romaine Hearts with Bacon & Celery Salt Ranch", ingredients: "
+Ingredients\r\n\r\n
+For the celery salt ranch\r\n
+
+    1 cup Primal Kitchen Foods Mayo\r\n
+    1⁄2 cup full-fat coconut milk\r\n
+    2 tablespoons lemon juice\r\n
+    3 tablespoons minced fresh parsley\r\n
+    2 tablespoons minced fresh chives\r\n
+    1 tablespoon minced fresh dill\r\n
+    1⁄2 teaspoon garlic powder\r\n
+    ½ teaspoon celery salt\r\n
+    pinch of fine sea salt and black pepper\r\n\r\n
+
+For the grilled romaine\r\n
+
+    4 pieces of bacon, chopped\r\n
+    2 tablespoons melted ghee\r\n
+    3 hearts of romaine, cut in half lengthwise\r\n
+    ¼ teaspoon fine sea salt\r\n
+    ¼ teaspoon garlic powder\r\n
+    ¼ teaspoon celery salt\r\n\r\n
+
+Instructions\r\n\r\n
+
+    1. In a blender, add all ingredients for the celery salt ranch until smooth. Place in a closed container in the fridge to chill.\r\n
+    2. Place bacon in a medium saute pan over medium heat. Cook until crispy then set aside on a paper towel to soak up excess fat.\r\n
+    3. Heat grill to high heat. Once hot, use a brush to brush the inside of each half of a romaine heart with melted ghee. Then sprinkle each half with salt, garlic powder, and celery salt. Place greased side down on the grill to cook for about 2-3 minutes. While cooking, grease once more on the top side then flip and cook for 1-2 more minutes.\r\n
+    4. Top each grilled romaine heart with ranch and bacon. Serve immediately!\r\n
+"}
+  ]
+)
